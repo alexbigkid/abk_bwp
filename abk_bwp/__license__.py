097464c14f2df48e2e64b775e5a741cfd72dcdbb
Alex Berger <alexbigkid@users.noreply.github.com>
2022-09-12 12:56:40 -0700

    e updated version. THis needs to be corrected

--- abk_bwp/__license__.py
+++ abk_bwp/__license__.py
@@ -1,4 +1,5 @@
 __author__ = "Alex Berger"
 __copyright__ = "Copyright (C) 2022 ABKcompany"
 __license__ = "GNU GENERAL PUBLIC LICENSE"
-__version__ = "0.0.2"
+# TODO: Need to get rid of the version. SHould be done on the fly
+__version__ = "0.0.3"
